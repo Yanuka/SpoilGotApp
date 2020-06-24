3c11bca9893710bc6d4f1cf4ceda4ff8ad934196
Yanuka <34684096+Yanuka@users.noreply.github.com>
2020-06-24 21:11:20 +0200

    TEST

--- src/test/java/com/fdorval/spoilgot/integration/IntegrationTest.java
+++ src/test/java/com/fdorval/spoilgot/integration/IntegrationTest.java
@@ -47,6 +47,15 @@ public class IntegrationTest {
     }
 
     @Test
+    public void shouldReturn200ifOK() throws Exception {
+        for (int i = 1; i <= 8; i++ ) {
+            assertThat(this.restTemplate.getForEntity("http://localhost:" + port + "/characters?season=" + i,
+                    String.class).getStatusCode()).isEqualTo(HttpStatus.OK);
+        }
+       
+    }
+
+    @Test
     public void shouldReturn404ifWrongUrl() throws Exception {
         assertThat(this.restTemplate.getForEntity("http://localhost:" + port + "/kamoulox",
                 String.class).getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
